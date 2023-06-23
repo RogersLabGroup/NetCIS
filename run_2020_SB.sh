5075c1d56a89df961d0af2e2a18c45f04c7f897e
Mathwe Fischbach <mathew_fischbach@yahoo.com>
2023-06-23 10:18:11 -0500

    small changes to run scripts

--- run_2020_SB.sh
+++ run_2020_SB.sh
@@ -22,16 +22,16 @@ npara=12
 output_prefix="2020_SB/all_files"
 bowtieIndex="/research/labs/immunology/rogerslm/tools/bowtie2_indexes/GRCm39/GRCm39"
 input="2020_SB/input.tsv"
-chrom_bed="2020_SB/keep-chroms.bed"
+# chrom_bed="toy-data/keep-chroms.bed"  # don't need chrom bed anymore, since I've personalized the mapper indexes
 irl_tpn="AAATTTGTGGAGTAGTTGAAAAACGAGTTTTAATGACTCCAACTTAAGTGTATGTAAACTTCCGACTTCAACTG"
 irr_tpn="GGATTAAATGTCAGGAATTGTGAAAAAGTGAGTTTAAATGTATTTGGCTAAGGTGTATGTAAACTTCCGACTTCAACTG"
 primer="GTAATACGACTCACTATAGGGCTCCGCTTAAGGGAC"
-python netcis/preprocess_reads.py -d $fastq -o $output_prefix -b $bowtieIndex -i $input -l $irl_tpn -r $irr_tpn -p $primer -t $ntasks -n $npara -t $ntasks -n $npara -c $chrom_bed
+python netcis/preprocess_reads.py -d $fastq -o $output_prefix -b $bowtieIndex -i $input -l $irl_tpn -r $irr_tpn -p $primer -t $ntasks -n $npara -t $ntasks -n $npara  # -c $chrom_bed
 
 njobs=$((ntasks * npara))
 mapP_thresh=0.05
-chrom_mapper="2020_SB/chrom_mapper.tsv"
-python netcis/preprocess_insertions.py -o $output_prefix -i $input -c $chrom_mapper -t $mapP_thresh -j $njobs
+# chrom_mapper="toy-data/chrom_mapper.tsv"
+python netcis/preprocess_insertions.py -o $output_prefix -i $input -t $mapP_thresh -j $njobs  # -c $chrom_mapper
 
 edge_thresh=50000
 python netcis/cis_networks.py -o $output_prefix -t $edge_thresh -j 4
--- run_toy-dataset.sh
+++ run_toy-dataset.sh
@@ -14,16 +14,16 @@ fastq="toy-data/2020_SB-fastq"
 output_prefix="toy-data/2020_SB"
 bowtieIndex="/research/labs/immunology/rogerslm/tools/bowtie2_indexes/GRCm39/GRCm39"
 input="toy-data/input.tsv"
-chrom_bed="toy-data/keep-chroms.bed"
+# chrom_bed="toy-data/keep-chroms.bed"  # don't need chrom bed anymore, since I've personalized the mapper indexes
 irl_tpn="AAATTTGTGGAGTAGTTGAAAAACGAGTTTTAATGACTCCAACTTAAGTGTATGTAAACTTCCGACTTCAACTG"
 irr_tpn="GGATTAAATGTCAGGAATTGTGAAAAAGTGAGTTTAAATGTATTTGGCTAAGGTGTATGTAAACTTCCGACTTCAACTG"
 primer="GTAATACGACTCACTATAGGGCTCCGCTTAAGGGAC"
-python netcis/preprocess_reads.py -d $fastq -o $output_prefix -b $bowtieIndex -i $input -l $irl_tpn -r $irr_tpn -p $primer -t $ntasks -n $npara -c $chrom_bed
+python netcis/preprocess_reads.py -d $fastq -o $output_prefix -b $bowtieIndex -i $input -l $irl_tpn -r $irr_tpn -p $primer -t $ntasks -n $npara  # -c $chrom_bed
 
 njobs=$((ntasks * npara))
 mapP_thresh=0.05
-chrom_mapper="toy-data/chrom_mapper.tsv"
-python netcis/preprocess_insertions.py -o $output_prefix -i $input -c $chrom_mapper -t $mapP_thresh -j $njobs
+# chrom_mapper="toy-data/chrom_mapper.tsv"
+python netcis/preprocess_insertions.py -o $output_prefix -i $input -t $mapP_thresh -j $njobs  # -c $chrom_mapper
 
 edge_thresh=50000
 python netcis/cis_networks.py -o $output_prefix -t $edge_thresh -j $njobs
