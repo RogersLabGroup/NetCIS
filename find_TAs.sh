6061d97571e25d6f6ee3c604cae3f0b93e1d9baf
Mathew Fischbach <mathew_fischbach@yahoo.com>
2023-09-07 13:17:36 -0500

    made find_TAs.sh flexible.

--- find_TAs.sh
+++ find_TAs.sh
@@ -4,12 +4,11 @@ set -u
 set -o pipefail
 
 # contains fasta files. ex.) chr1.fa,  chr12.fa, etc.
-file_dir=/research/labs/immunology/rogerslm/m277102/projects/2023_SB/ref_data/seq_kit_ta_files
-output_dir=/research/labs/immunology/rogerslm/m277102/projects/2023_SB/ref_data/ta_files
+file_dir=$1
+output_dir=$2
 
-files=$(ls $file_dir)
-for file in $files
+files=(chr1.fa chr2.fa chr3.fa chr4.fa chr5.fa chr6.fa chr7.fa chr8.fa chr9.fa chr10.fa chr11.fa chr12.fa chr13.fa chr14.fa chr15.fa chr16.fa chr17.fa chr18.fa chr19.fa chrX.fa chrY.fa chrM.fa)
+for file in "${files[@]}"
 do
-    echo "$file"
     seqkit locate -P -p TA --bed < "$file_dir/$file" > "$output_dir/$file.bed"
 done
